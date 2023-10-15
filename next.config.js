8560da4c137ce8dc769ded02263f025f1cac73ea
Soham <sohampatel2050@gmail.com>
2023-10-15 17:14:38 -0400

    change in env

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
         domains:["cdn.imagin.studio"]
     },
     env:{
-        NEXT_PUBLIC_RAPID_API_KEY:"dc67ef9247mshc850f572baa19a1p17903bjsn128d84cc6626",
-        NEXT_PUBLIC_IMAGIN_API_KEY:"hrjavascript-mastery"
+        NEXT_PUBLIC_RAPID_API_KEY:"",
+        NEXT_PUBLIC_IMAGIN_API_KEY:""
     }
 }
 
