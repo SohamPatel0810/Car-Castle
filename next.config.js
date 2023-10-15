66d47e781e527e3bf48848159925b40106cd3b81
Soham <sohampatel2050@gmail.com>
2023-10-15 17:05:24 -0400

    done

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:["cdn.imagin.studio"]
+    },
+    env:{
+        NEXT_PUBLIC_RAPID_API_KEY:"dc67ef9247mshc850f572baa19a1p17903bjsn128d84cc6626",
+        NEXT_PUBLIC_IMAGIN_API_KEY:"hrjavascript-mastery"
+    }
+}
+
+module.exports = nextConfig
